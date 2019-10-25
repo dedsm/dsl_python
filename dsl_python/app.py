f90dc61f56b452650912845831306628cc54aa53
David de Sousa <davidesousa@gmail.com>
2019-10-25 12:58:21 +0200

    Sum with 3 parameters

--- dsl_python/app.py
+++ dsl_python/app.py
@@ -15,3 +15,16 @@ def sum():
     b = float(request.args.get('param2'))
 
     return jsonify(result=a+b)
+
+
+@app.route('/sum2')
+def sum2():
+    # skip validations because reasons
+    a = float(request.args.get('param1'))
+    b = float(request.args.get('param2'))
+
+    c = 0
+    if 'param3' in request.args:
+        c = float(request.args.get('param3'))
+
+    return jsonify(result=a+b+c)
