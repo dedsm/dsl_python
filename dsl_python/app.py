093e8fa2b8c4acaa7301bc47b3408c5b493f7db4
David de Sousa <davidesousa@gmail.com>
2019-10-25 13:11:07 +0200

    Add JSON operator

--- dsl_python/app.py
+++ dsl_python/app.py
@@ -1,4 +1,7 @@
-from flask import Flask, request, jsonify
+import json
+import operator
+
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -14,7 +17,7 @@ def sum():
     a = float(request.args.get('param1'))
     b = float(request.args.get('param2'))
 
-    return jsonify(result=a+b)
+    return jsonify(result=a + b)
 
 
 @app.route('/sum2')
@@ -27,4 +30,32 @@ def sum2():
     if 'param3' in request.args:
         c = float(request.args.get('param3'))
 
-    return jsonify(result=a+b+c)
+    return jsonify(result=a + b + c)
+
+
+def process_operation(operation):
+    operators = {
+        '*': operator.mul,
+        '/': operator.truediv,
+        '+': operator.add,
+        '-': operator.sub
+    }
+
+    param1 = operation['param1']
+    param2 = operation['param2']
+    operator_function = operators[operation['operator']]
+
+    if type(param1) is dict:
+        param1 = process_operation(param1)
+
+    if type(param2) is dict:
+        param2 = process_operation(param2)
+
+    return operator_function(param1, param2)
+
+
+@app.route('/math')
+def math():
+    operation = json.loads(request.args.get('operation'))
+
+    return jsonify(result=process_operation(operation))
